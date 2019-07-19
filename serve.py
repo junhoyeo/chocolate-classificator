21bca93d587cfe2bad7f9b7913e11574cb1afe0d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-20 07:00:35 +0900

    fix: render images with base64

--- serve.py
+++ serve.py
@@ -3,6 +3,7 @@ from PIL import Image
 from io import BytesIO
 import torch
 from model import Net, transform
+import base64
 import time
 
 app = Flask(__name__)
@@ -21,24 +22,23 @@ def home():
             return render_template(
                 'index.html',
                 error='이미지 형식이 잘못되었어요. png나 jpg 확장자를 가진 파일만 업로드할 수 있어요.')
-        image_first_name = image.filename
-        image_name = 'uploads/{}-{}'.format(int(time.time()), image.filename)
-        image_path = './static/{}'.format(image_name)
-        image.save(image_path)
+        image_name = image.filename
+        image_bytes = BytesIO(image.read())
         try:
-            image = Image.open(image_path)
+            image = Image.open(image_bytes)
             input = transform(image)
             input = input.view(1, 3, 224, 224)
             output = model(input)
             prediction = int(torch.max(output.data, 1)[1].numpy())
         except BaseException:
             return render_template('index.html', error='이미지 처리 중 에러가 발생했어요.')
-        return render_template('index.html',
-                               success=True,
-                               result=prediction,
-                               image=image_name,
-                               filename=image_first_name
-                               )
+        return render_template(
+            'index.html',
+            success=True,
+            result=prediction,
+            image=base64.b64encode(
+                image_bytes.getvalue()).decode('ascii'),
+            filename=image_name)
     return render_template('index.html')
 
 
